9e9c14d33c65f6b5630759e8fd6aeecd4f75b13b
hohserg1 <hohserg1@yandex.ru>
2026-07-01 23:57:26 +0300

    added scythe setting for forester building
    bc its too hard to make adaptive hoe/scythe usage

--- src/main/java/net/ckeeze/terrafirmacolonies/mixin/EntityAIWorkLumberjackMixin.java
+++ src/main/java/net/ckeeze/terrafirmacolonies/mixin/EntityAIWorkLumberjackMixin.java
@@ -2,33 +2,40 @@ package net.ckeeze.terrafirmacolonies.mixin;
 
 import com.minecolonies.api.equipment.ModEquipmentTypes;
 import com.minecolonies.api.equipment.registry.EquipmentTypeEntry;
+import com.minecolonies.core.colony.buildings.workerbuildings.BuildingLumberjack;
+import com.minecolonies.core.colony.jobs.JobLumberjack;
+import com.minecolonies.core.entity.ai.workers.crafting.AbstractEntityAICrafting;
 import com.minecolonies.core.entity.ai.workers.production.EntityAIWorkLumberjack;
 import net.ckeeze.terrafirmacolonies.api.TFCEquipmentTypes;
+import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.NotNull;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import static net.ckeeze.terrafirmacolonies.api.CustomSettings.USE_SCYTHE;
+
 @Mixin(value = EntityAIWorkLumberjack.class, remap = false)
-public abstract class EntityAIWorkLumberjackMixin {
+public abstract class EntityAIWorkLumberjackMixin extends AbstractEntityAICrafting<JobLumberjack, BuildingLumberjack> {
+
+    public EntityAIWorkLumberjackMixin(@NotNull JobLumberjack job) {
+        super(job);
+    }
 
     @Redirect(
         method = "prepareForWoodcutting",
         at = @At(
-            value = "INVOKE",
-            target = "Lcom/minecolonies/core/entity/ai/workers/production/EntityAIWorkLumberjack;checkForToolOrWeapon(Lcom/minecolonies/api/equipment/registry/EquipmentTypeEntry;)Z",
-            ordinal = 1
+            value = "FIELD",
+            target = "Lcom/minecolonies/api/equipment/ModEquipmentTypes;hoe:Lnet/minecraftforge/registries/RegistryObject;",
+            opcode = Opcodes.GETSTATIC
         )
     )
-    public boolean useScythe(EntityAIWorkLumberjack self, EquipmentTypeEntry equipmentType) {
-        if (equipmentType == ModEquipmentTypes.hoe.get()) {
-            boolean fail = self.checkForToolOrWeapon(TFCEquipmentTypes.tfcscythe.get());
-            if (fail) {
-                return self.checkForToolOrWeapon(ModEquipmentTypes.hoe.get());
-            } else {
-                return false;
-            }
-        }
-        return self.checkForToolOrWeapon(equipmentType);
+    public RegistryObject<EquipmentTypeEntry> useScythe() {
+        if (building.getSetting(USE_SCYTHE).getValue())
+            return TFCEquipmentTypes.tfcscythe;
+        else
+            return ModEquipmentTypes.hoe;
     }
 
 }
